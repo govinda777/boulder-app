73d19f947da56a865630446c2eabffd9d589cdd6
Govinda <govinda777@gmail.com>
2022-09-08 10:28:14 -0300

    atualizacao

--- src/app/modules/application/dashboard/card/card.ts
+++ src/app/modules/application/dashboard/card/card.ts
@@ -7,7 +7,7 @@ export class DashboardCard {
   
     constructor() {
 
-      this.img = 'https://rockcontent.com/br/wp-content/uploads/sites/2/2020/02/teste-de-lideran%C3%A7a.png';
+      this.img = './assets/params/images/logo/logo.png';
       this.title = "Title - old - old" ;
       this.linkRef = '#';
       this.desc = '111111';
